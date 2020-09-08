58f52f7b614e299e4cbe0497c1405614ea86ec45
Avery Nguyen <averynguyen@Averys-MacBook-Air.local>
2020-09-08 12:14:26 -0600

    sidebar function

--- App.js
+++ App.js
@@ -4,33 +4,89 @@ import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator, DrawerItem } from '@react-navigation/drawer';
 
 
-function About({ navigation }) {
+function HomeScreen({ navigation }) {
+  return (
+    <ImageBackground source={require('./img/mohammadreza-alidoost-sUoXs_lUhug-unsplash.jpg')} style={{width: '100%', height: '100%'}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate('About')}
+      />
+      <Button
+        title="Go to Stills"
+        onPress={() => navigation.navigate('Stills')}
+      />
+      </View>
+    </ImageBackground>
+  );
+}
+
+function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text> About </Text>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+function AboutScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>About</Text>
+    </View>
+  );
+}
+
+function StillsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Stills</Text>
     </View>
   );
 }
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App({ navigation }) {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <ImageBackground source={require('./img/mohammadreza-alidoost-sUoXs_lUhug-unsplash.jpg')} style={{width: '100%', height: '100%'}}>
-          <Button title= "Contact" onPress={() => alert('Contact')}/>
-          <Button title= "About" onPress={() => navigation.navigate('About')}/>
-          <Button title= "Stills" onPress={() => alert('Stills')}/>
-          <Stack.Navigator>
-             <Stack.Screen name="About" component={About} />
-          </Stack.Navigator>
-          <Text style={styles.title}>AN Photography</Text>
-          <StatusBar style="auto" />
-        </ImageBackground>
-      </View>
+      {/* <Stack.Navigator>
+      <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Menu"
+                color="#00cc00"
+              />
+            ),
+          }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Stills" component={StillsScreen} />
+      </Stack.Navigator> */}
+      <DrawerItem
+        label="Toggle drawer"
+        onPress={() => props.navigation.toggleDrawer()}
+      />
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Details" component={DetailsScreen} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="Stills" component={StillsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
@@ -45,5 +101,6 @@ const styles = StyleSheet.create({
   title: {
     margin: 'auto',
     color: 'white',
-  }
+  },
+  background: ''
 });
